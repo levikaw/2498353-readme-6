44ee227d2cc0de8ac2b614a80e17d390d5af6e97
levikaw <levkin_dmitrii@mail.ru>
2024-03-26 21:19:39 +0500

    feat(apps): add sketch

---
+++ project/apps/controller/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'controller',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/controller'
+};
