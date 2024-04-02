674bf041415fd9340821e6c978e889933ebf93bd
levikaw <levkin_dmitrii@mail.ru>
2024-04-02 08:35:43 +0500

    feat(lib): init data-access

---
+++ project/libs/shared/data-access/src/repository/repository.interface.ts
@@ -0,0 +1,29 @@
+import { Entity } from '@project/core';
+
+export interface Repository<T extends Entity> {
+  /**
+   * Поиск entity по id
+   * @param {T['id']} id
+   * @returns {Promise<T>}
+   */
+  findById(id: T['id']): Promise<T>;
+
+  /**
+   * Создание entity
+   * @param {T} entity
+   * @returns {Promise<T>}
+   */
+  create(entity: T): Promise<T>;
+
+  /**
+   * Обновление entity
+   * @param {T} entity
+   */
+  update(entity: T): Promise<void>;
+
+  /**
+   * Удаление entity по id
+   * @param {T['id']} id
+   */
+  deleteById(id: T['id']): Promise<void>;
+}
