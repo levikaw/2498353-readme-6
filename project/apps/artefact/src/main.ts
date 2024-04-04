20c3060dfaea7efc9c618328e51fa54f1d9d83ff
levikaw <levkin_dmitrii@mail.ru>
2024-04-04 16:42:16 +0500

    feat(api): add swagger

--- project/apps/artefact/src/main.ts
+++ project/apps/artefact/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { MainModule } from './main.module';
 
 async function bootstrap() {
@@ -12,6 +13,18 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('artefact api')
+    .setDescription('The artefact API description')
+    .setVersion('1.0')
+    .addTag('artefact')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
--- project/apps/auth/src/main.ts
+++ project/apps/auth/src/main.ts
@@ -5,15 +5,26 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthModule } from './app/auth.module';
-
-// import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('auth api')
+    .setDescription('The auth API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
