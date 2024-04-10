4bba7ad306651e3e476bbd5ecd69c39be55177d2
levikaw <levkin_dmitrii@mail.ru>
2024-04-10 10:25:52 +0500

    fix: fix for-loop upload files

--- project/apps/file/src/app/file.service.ts
+++ project/apps/file/src/app/file.service.ts
@@ -6,14 +6,13 @@ export class FileService {
   constructor(private readonly fileAccessRepository: FileAccessRepository) {}
 
   public async upload(files: Express.Multer.File[], userId: string): Promise<string[]> {
-    const uploadedFiles: string[] = [];
-    for (const file of files) {
-      const fileEntity = new FileAccessEntity({ name: file.originalname, content: file.buffer.toString('base64'), userId });
-      const processedFile = await this.fileAccessRepository.save(fileEntity);
-      uploadedFiles.push(processedFile.id);
-    }
-
-    return uploadedFiles;
+    return await Promise.all(
+      files.map(async (file) => {
+        const fileEntity = new FileAccessEntity({ name: file.originalname, content: file.buffer.toString('base64'), userId });
+        const processedFile = await this.fileAccessRepository.save(fileEntity);
+        return processedFile.id;
+      }),
+    );
   }
 
   public async download(id: string): Promise<Buffer> {
