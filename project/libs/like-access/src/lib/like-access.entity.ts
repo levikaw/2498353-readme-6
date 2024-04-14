f8a4e078d2b1f22f798061d5657d37d005a2c2d5
levikaw <levkin_dmitrii@mail.ru>
2024-04-14 20:00:49 +0500

    feat(like): add postgres repository

--- project/libs/like-access/src/lib/like-access.entity.ts
+++ project/libs/like-access/src/lib/like-access.entity.ts
@@ -1,9 +1,14 @@
 import { BaseEntity, StorableEntity } from '@project/core';
+import { isNotEmpty } from 'class-validator';
 import { UserLike } from './types/like.interface';
 
 export class LikeAccessEntity extends BaseEntity implements StorableEntity<UserLike> {
   constructor(like: UserLike) {
     super();
+
+    if (!isNotEmpty(like)) {
+      return undefined;
+    }
 
     this.id = like.id;
     this.createdAt = like.createdAt;
--- project/libs/like-access/src/lib/like-access.repository.ts
+++ project/libs/like-access/src/lib/like-access.repository.ts
@@ -1,24 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMemoryRepository } from '@project/data-access';
+import { BasePostgresRepository } from '@project/data-access';
+import { PrismaService } from '@project/prisma';
 import { LikeAccessEntity } from './like-access.entity';
 import { LikeAccessFactory } from './like-access.factory';
 
 @Injectable()
-export class LikeAccessRepository extends BaseMemoryRepository<LikeAccessEntity> {
-  constructor(entityFactory: LikeAccessFactory) {
-    super(entityFactory);
+export class LikeAccessRepository extends BasePostgresRepository<LikeAccessEntity> {
+  constructor(entityFactory: LikeAccessFactory, readonly dataSource: PrismaService) {
+    super(entityFactory, dataSource);
   }
 
-  public async findByPostId(postId: string): Promise<LikeAccessEntity[]> {
-    return Array.from(this.entities.values())
-      .filter((entity) => entity.postId === postId && !entity.deletedAt)
-      .map((c) => this.entityFactory.createEntity(c));
+  public async save(entity: LikeAccessEntity): Promise<LikeAccessEntity> {
+    return this.dataSource.like
+      .create({
+        data: entity.toObject(),
+      })
+      .then((resp) => this.entityFactory.createEntity(resp));
+  }
+
+  public async deleteById(id: string): Promise<void> {
+    await this.dataSource.like.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  public async findByPostId(postId: string, skip?: number, take?: number): Promise<LikeAccessEntity[]> {
+    return this.dataSource.like
+      .findMany({
+        where: {
+          postId,
+        },
+        skip,
+        take,
+      })
+      .then((resp) => resp.map((c) => this.entityFactory.createEntity(c)));
   }
 
   public async findByPostIdUserId(postId: string, userId: string): Promise<LikeAccessEntity> {
-    const like = Array.from(this.entities.values()).find(
-      (entity) => entity.postId === postId && entity.userId === userId && !entity.deletedAt,
-    );
-    return !!like ? this.entityFactory.createEntity(like) : null;
+    return this.dataSource.like
+      .findFirst({
+        where: {
+          userId,
+          postId,
+        },
+      })
+      .then((resp) => this.entityFactory.createEntity(resp));
   }
 }
