64da54d518370592a27dca6d877a10e89b052001
levikaw <levkin_dmitrii@mail.ru>
2024-04-14 20:03:19 +0500

    feat: base postgres repository

--- project/libs/data-access/src/index.ts
+++ project/libs/data-access/src/index.ts
@@ -1,2 +1,3 @@
-export { BaseMemoryRepository } from './lib/base-memory.repository';
-export { BaseMongoRepository } from './lib/base-mongo.repository';
+export * from './lib/base-memory.repository';
+export * from './lib/base-mongo.repository';
+export * from './lib/base-postgres.repository';
