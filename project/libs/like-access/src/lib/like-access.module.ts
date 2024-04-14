2b0a50eaebaf73d12829b373154562438c45e305
levikaw <levkin_dmitrii@mail.ru>
2024-04-14 20:00:49 +0500

    feat(like): add postgres repository

--- project/libs/like-access/src/lib/like-access.module.ts
+++ project/libs/like-access/src/lib/like-access.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LikeAccessRepository } from './like-access.repository';
 import { LikeAccessFactory } from './like-access.factory';
+import { PrismaModule } from '@project/prisma';
 
 @Module({
+  imports: [PrismaModule],
   providers: [LikeAccessRepository, LikeAccessFactory],
   exports: [LikeAccessRepository],
 })
