79e764a3399bc8f2c862714331311b68846caa3b
levikaw <levkin_dmitrii@mail.ru>
2024-04-14 20:00:01 +0500

    feat(comment): add postgres repository

--- project/apps/artefact/src/comment/comment.service.ts
+++ project/apps/artefact/src/comment/comment.service.ts
@@ -6,6 +6,7 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 export class CommentService {
   constructor(private readonly commentAccessRepository: CommentAccessRepository) {}
 
+  // TODO: pagination
   public async findCommentsByPostId(postId: string): Promise<Commentary[]> {
     return this.commentAccessRepository.findCommentsByPostId(postId).then((resp) => resp.map((c) => c.toObject()));
   }
--- project/libs/comment-access/src/lib/comment-access.module.ts
+++ project/libs/comment-access/src/lib/comment-access.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentAccessRepository } from './comment-access.repository';
 import { CommentAccessFactory } from './comment-access.factory';
+import { PrismaModule } from '@project/prisma';
 
 @Module({
+  imports: [PrismaModule],
   providers: [CommentAccessRepository, CommentAccessFactory],
   exports: [CommentAccessRepository],
 })
