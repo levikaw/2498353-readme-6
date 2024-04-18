2ef2f3668a948771655fab47b6c4125059797289
levikaw <levkin_dmitrii@mail.ru>
2024-04-18 19:10:51 +0500

    fix: configs

--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -7,12 +7,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { setUpSwaggerModule } from '@project/swagger';
 import { UserModule } from './app/user.module';
+import { ConfigService } from '@nestjs/config';
+import { USERS_ALIAS } from '@project/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get(`${USERS_ALIAS}.port`);
 
   setUpSwaggerModule<UserModule>(app, 'user');
 
